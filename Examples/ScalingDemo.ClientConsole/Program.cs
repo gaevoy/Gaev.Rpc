4efc1f39937454fb2917806dfc534e999be6259d
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2015-12-03 14:17:49 +0200

    Move rebus configuration into separated method

--- Examples/ScalingDemo.ClientConsole/Program.cs
+++ Examples/ScalingDemo.ClientConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Gaev.RebbitMqCommandBus.Shared;
 using Rebus.Activation;
+using Rebus.Bus;
 using Rebus.Config;
 using Rebus.Logging;
 using Rebus.RabbitMq;
@@ -17,11 +18,7 @@ namespace ScalingDemo.ClientConsole
         {
             using (var activator = new BuiltinHandlerActivator())
             {
-                Configure.With(activator)
-                    .Logging(e => e.ColoredConsole(LogLevel.Warn))
-                    .Transport(t => t.UseRabbitMq("amqp://localhost", "ScalingDemo-Requestor-" + NodeId))
-                    .Routing(e => e.TypeBased())
-                    .Start();
+                StartBus(activator);
 
                 IRequestor requestor = new RebusRequestor(activator);
 
@@ -30,10 +27,20 @@ namespace ScalingDemo.ClientConsole
                 {
                     var cmd = Console.ReadLine();
                     if (cmd == "exit") return;
+
                     var res = (PongMessage)requestor.Ask(new PingMessage { Sender = NodeId, Payload = cmd }).Result;
                     Console.WriteLine("Server {0} received", res.Sender);
                 }
             }
+        }
+
+        static IBus StartBus(BuiltinHandlerActivator activator)
+        {
+            return Configure.With(activator)
+                .Logging(e => e.ColoredConsole(LogLevel.Warn))
+                .Transport(t => t.UseRabbitMq("amqp://localhost", "ScalingDemo-Requestor-" + NodeId))
+                .Routing(e => e.TypeBased())
+                .Start();
         }
     }
 }
--- Examples/ScalingDemo.ServerConsole/Program.cs
+++ Examples/ScalingDemo.ServerConsole/Program.cs
@@ -2,10 +2,12 @@
 using System.Threading.Tasks;
 using Gaev.RebbitMqCommandBus.Shared;
 using Rebus.Activation;
+using Rebus.Bus;
 using Rebus.Config;
 using Rebus.Logging;
 using Rebus.RabbitMq;
 using Rebus.Routing.TypeBased;
+using Rebus.Rpc;
 using Rebus.Rpc.Impl;
 
 namespace ScalingDemo.ServerConsole
@@ -17,24 +19,29 @@ namespace ScalingDemo.ServerConsole
         {
             using (var activator = new BuiltinHandlerActivator())
             {
-                var bus = Configure.With(activator)
-                    .Logging(e => e.ColoredConsole(LogLevel.Warn))
-                    .Transport(t => t.UseRabbitMq("amqp://localhost", "ScalingDemo-Responder"))
-                    .Routing(e => e.TypeBased())
-                    .Start();
+                var bus = StartBus(activator);
 
-                var responder = new RebusResponder(activator, bus);
+                IResponder responder = new RebusResponder(activator, bus);
                 responder.On<PingMessage>(OnPing);
 
                 Console.WriteLine("Server {0}. Press enter to quit", NodeId);
                 Console.ReadLine();
             }
         }
-
+        
         static Task<object> OnPing(PingMessage msg)
         {
             Console.WriteLine("Server received {0}", msg.Payload);
             return Task.FromResult<object>(new PongMessage { Sender = NodeId });
+        }
+        
+        static IBus StartBus(BuiltinHandlerActivator activator)
+        {
+            return Configure.With(activator)
+                .Logging(e => e.ColoredConsole(LogLevel.Warn))
+                .Transport(t => t.UseRabbitMq("amqp://localhost", "ScalingDemo-Responder"))
+                .Routing(e => e.TypeBased())
+                .Start();
         }
     }
 }
